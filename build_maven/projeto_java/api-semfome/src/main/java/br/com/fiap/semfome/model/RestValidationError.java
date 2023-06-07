33d2120f6303919e919489ab65032152e7124d76
DabGias <gabrielfurlaneti@gmail.com>
2023-06-07 14:08:53 -0300

    correções

--- build_maven/projeto_java/api-semfome/src/main/java/br/com/fiap/semfome/model/RestValidationError.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.semfome.model;
-
-public record RestValidationError(String field, String message) {
-    
-}
--- build_maven/projeto_java/api-semfome/src/main/java/br/com/fiap/semfome/repository/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.semfome.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.semfome.model.Empresa;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-    
-}
